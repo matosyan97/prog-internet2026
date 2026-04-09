67db2f82e74450081b4d7080acc2f0a4b9727043
fleithpi <ceco1995@gmail.com>
2026-04-09 20:53:56 -0300

    js+

--- Área de Trabalho/prog-internet2026-main/Área de Trabalho/prog-internet2026-main/Área de Trabalho/prog-internet2026-main/Área de Trabalho/roteiro02/assets/js/script.js
+++ Área de Trabalho/prog-internet2026-main/Área de Trabalho/prog-internet2026-main/Área de Trabalho/prog-internet2026-main/Área de Trabalho/roteiro02/assets/js/script.js
@@ -1,1 +1,24 @@
-alert('Hello, welcome to my mini-site!')
+// alert('Hello, welcome to my mini-site!')
+// This is a comment in JavaScript. It will not be executed by the browser.
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting and refreshing the page
+    console.log('Form submitted!'); // Log a message to the console
+    
+    const nome = document.getElementById('nome').value;
+    console.log(nome);
+    alert(nome);
+
+    if (nome.length < 3) {
+        alert("O nome deve ter pelo menos 3 caracteres.");
+        return;
+    }
+
+    if (!email.includes("@")) {
+        alert("Email inválido.");
+        return;
+}
+
+
+});
